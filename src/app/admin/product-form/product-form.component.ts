c6f525592939c8256be361a5e6f35c110875ca75
Radoslaw Subczynski <kozak221@gmail.com>
2019-09-08 21:40:16 +0200

    add edit and delete products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -11,17 +12,34 @@ import { Router } from '@angular/router';
 export class ProductFormComponent implements OnInit {
 
   categories$;
+  product = {};
+  id;
 
-  constructor(private router: Router, categoryService: CategoryService, private productService: ProductService) {
+  constructor(private router: Router, categoryService: CategoryService, private productService: ProductService,
+    private route: ActivatedRoute) {
     this.categories$ = categoryService.getCategories();
+
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService.getProduct(this.id).pipe(
+        take(1)).subscribe(p => this.product = p);
+    }
   }
 
   ngOnInit() {
   }
 
   save(product) {
-    this.productService.create(product);
+    if (this.id) { this.productService.update(this.id, product); } else { this.productService.create(product); }
     this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if (!confirm('Are you sure want to delete this products?')) { return; } {
+      this.productService.delete(this.id)
+      this.router.navigate(['/admin/products']);
+    }
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,8 @@
+import { Observable } from 'rxjs';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Product } from './models/app-product';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,28 @@ export class ProductService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create(product) {
+  create(product: Product) {
     return this.db.list('/products').push(product);
   }
+
+  getAll() {
+    return this.db.list('/products').snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() }));
+      }));
+  }
+
+  getProduct(productId: string): Observable<Product> {
+    return this.db.object<Product>('/products/' + productId).valueChanges();
+  }
+
+  update(productId: string, product: Product) {
+    return this.db.object<Product>('/products/' + productId).update(product);
+  }
+
+  delete(productId: string) {
+    return this.db.object<Product>('/products/' + productId).remove()
+  }
 }
+
